20ee4d2a85349199f8dbd5a3e091712c68cf8da9
MagnusBM94 <magnusbm94@gmail.com>
2021-03-30 14:39:05 +0200

    login

--- src/App.js
+++ src/App.js
@@ -2,19 +2,22 @@ import React, { Component } from "react";
 import Game from "./chess/game/game.js";
 import Signup from "./authentication/Signup";
 import { Container } from "react-bootstrap";
+import { AuthProvider } from "./authentication/AuthContext";
 
 class App extends Component {
   render() {
     return (
       <div>
-        <Container
-          className="d-flex align-items center justify-content-center"
-          style={{ minHeight: "100vh" }}
-        >
-          <div className="w-100" style={{ maxWidth: "400px" }}>
-            <Signup />
-          </div>
-        </Container>
+        <AuthProvider>
+          <Container
+            className="d-flex align-items center justify-content-center"
+            style={{ minHeight: "100vh" }}
+          >
+            <div className="w-100" style={{ maxWidth: "400px" }}>
+              <Signup />
+            </div>
+          </Container>
+        </AuthProvider>
         <div id="boardsContainer">
           <Game />
         </div>
